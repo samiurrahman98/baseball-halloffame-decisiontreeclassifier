ffef9cf779633f9215b8a2d87868eaf9ca4f82ee
Patrick Liu <pcjl@users.noreply.github.com>
2019-11-22 15:23:37 -0500

    Update classifier

--- part1/classifier.m
+++ part1/classifier.m
@@ -1,4 +1,6 @@
 T = readtable('train_sample.csv');
+T_C = readtable('train_sample_classification.csv');
 
-tc = fitctree(T, Y)
-YP = predict(tc, data)
+DT = fitctree(T(:,2:), T_C(:,2));
+view(DT, 'mode', 'graph')
+// YP = predict(tc, data);
