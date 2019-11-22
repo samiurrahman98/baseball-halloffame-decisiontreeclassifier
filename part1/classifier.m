a3357d4679e6b5be60fe0379d1bb0b5d725cb34f
Patrick Liu <pcjl@users.noreply.github.com>
2019-11-22 15:49:20 -0500

    Add classifier

--- part1/classifier.m
+++ part1/classifier.m
@@ -1,6 +1,9 @@
 T = readtable('train_sample.csv');
 T_C = readtable('train_sample_classification.csv');
 
-DT = fitctree(T(:,2:), T_C(:,2));
-view(DT, 'mode', 'graph')
-// YP = predict(tc, data);
+DT = fitctree(T(:,2:end), T_C(:,2));
+
+T = readtable('test_sample.csv');
+T_C = predict(DT, T(:,2:end));
+
+csvwrite('predictions.csv', T_C);
