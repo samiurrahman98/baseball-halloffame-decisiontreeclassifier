f5a133d57bd66fd359b6bebb45bbc72e43b63c02
Patrick Liu <pcjl@users.noreply.github.com>
2019-11-22 15:54:13 -0500

    Move data files

--- part1/train_test_split.py
+++ part1/train_test_split.py
@@ -3,17 +3,17 @@ from sklearn.model_selection import train_test_split
 
 
 def main():
-    data = pd.read_csv("data.csv", index_col=0)
+    data = pd.read_csv("data/data.csv", index_col=0)
 
     y = data.classification
     X = data.drop("classification", axis=1)
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-    X_train.to_csv("train_sample.csv")
-    y_train.to_csv("train_sample_classification.csv", header=True)
-    X_test.to_csv("test_sample.csv")
-    y_test.to_csv("test_sample_classification.csv", header=True)
+    X_train.to_csv("data/train_sample.csv")
+    y_train.to_csv("data/train_sample_classifications.csv", header=True)
+    X_test.to_csv("data/test_sample.csv")
+    y_test.to_csv("data/test_sample_classifications.csv", header=True)
 
 
 if __name__ == '__main__':
